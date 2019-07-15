01684443236d2ecca4585395e8a77aa7a3187ade
timroach <tim.roach1@gmail.com>
2019-07-14 18:53:00 -0700

    Removed hardcoding of key types

--- src/takeout.py
+++ src/takeout.py
@@ -4,9 +4,25 @@ class InputReader:
 
     def __init__(self, filename):
         self.filename = filename
+        # Stores dicts of JSON data parsed from input file
         self.resultlist = []
+        # Unique URL formats stored
+        self.urltypes = set()
+        # Dict with indexes of unique sets of JSON keys,
+        # contents of which are the dicts of each JSON
+        # with this key format
+        self.keylists = {}
 
     def readjsonfile(self):
         with open(self.filename) as jsonfile:
             self.resultlist = json.load(jsonfile)
+
+
+    def fillkeylists(self):
+        for item in self.keysets:
+            self.keylists[item] = []
+        for item in self.resultlist:
+            self.keylists[tuple(item.keys())].append(item)
+
+
 
