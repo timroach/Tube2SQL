43ea75d304a6b29f03fbb8a56449e66af97386de
timroach <tim.roach1@gmail.com>
2019-07-14 19:26:22 -0700

    Made tests for key presence modular

--- test/test_takout_fileread.py
+++ test/test_takout_fileread.py
@@ -2,7 +2,7 @@ import pytest
 from src import takeout
 
 # List of .json files to test
-test_files = {"../inputfiles/watch-history.json",}
+test_files = {"../inputfiles/watch-history.json", "testtakeoutfile.json"}
 
 # Tests all entries in test_files produce a
 # JSON file with the minimum set of properties per video
@@ -11,18 +11,22 @@ def test_file_import(test_file):
     testjsonfile = takeout.InputReader(test_file)
     testjsonfile.readjsonfile()
     testjsonfile.fillkeylists()
-# Three possible sets of dict keys for each JSON watch history entry
-    testkeys1 = ["header", "title", "titleUrl", "subtitles", "time", "products"]
-# Set 2 is for videos which are "unavailable", only URL and timestamp are stored
-    testkeys2 = ['header', 'title', 'titleUrl', 'time', 'products']
-# Set 3 is for videos which have been removed
-    testkeys3 = ['header', 'title', 'time', 'products']
+    # Three possible sets of dict keys for each JSON watch history entry
+    testkeys = [
+        # Set 1 is full set with all properties
+        ("header", "title", "titleUrl", "subtitles", "time", "products"),
+        # Set 2 is for videos which are "unavailable", only URL and timestamp are stored
+        ('header', 'title', 'titleUrl', 'time', 'products'),
+        # Set 3 is for videos which have been removed
+        ('header', 'title', 'time', 'products')]
 
     def keyexists(dict, key):
         return key in dict.keys()
 
-    for dictitem in testjsonfile.resultlist:
-        for item in testkeys3:
-            assert keyexists(dictitem, item)
+    keysgood = False
+    for item in testkeys:
+        if keyexists(testjsonfile.keylists, item):
+            keysgood = True
+    assert keysgood
 
 
