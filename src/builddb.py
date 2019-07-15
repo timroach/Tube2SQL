b8b5dd901283799eaa220d963f0f5c48043fcd4e
timroach@gmail.com <tim.roach1@gmail.com>
2019-07-15 03:58:40 -0700

    Create db automatically, use existing if found using BuildDB.opendb(), also with test

--- src/builddb.py
+++ src/builddb.py
@@ -3,22 +3,25 @@ import os
 import fnmatch
 import datetime
 
+
 class BuildDB:
 
-    def __init__(self):
+    def __init__(self, name):
         self.path = '../db/'
+        self.name = name
+        self.filename = ""
         self.existingdbs = []
         for file in os.listdir(self.path):
-            if fnmatch.fnmatch(file, '*.db'):
+            if fnmatch.fnmatch(file, name + '*.db'):
                 self.existingdbs.append(file)
 
     # Returns SQLite connection object for existing db
     # in /db/ directory, if none exist, creates new one
-    def opendb(self, name):
+    def opendb(self):
         if self.existingdbs:
-            filename = self.existingdbs[0]
+            self.filename = self.existingdbs[0]
         else:
             timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-            filename = name + timestamp + ".db"
-        return sqlite3.connect(filename)
+            self.filename = self.path + self.name + timestamp + ".db"
+        return sqlite3.connect(self.filename)
 
